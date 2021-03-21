85bc9cd97cbad41e81f52c2381f392a16744e00a
Nam Nguyen <namnguyen@NamNguyens-MacBook-Pro.local>
2021-03-21 09:56:36 +0700

    COde to init ec2

--- app.py
+++ app.py
@@ -2,11 +2,17 @@
 
 from aws_cdk import core
 from webapp.vpc import VPCStack
+from webapp.ec2 import Ec2Stack
+import yaml
 
 selected_env = core.Environment(account="658564819138", region="ap-southeast-1")
 
 app = core.App()
 
 vpcStack = VPCStack(app, 'vpc', env=selected_env)
+config = yaml.load(open('infra.yaml'), Loader=yaml.Loader)
+app = core.App()
+vpc = VPCStack(app,'vpc', env=selected_env)
+ec2 = Ec2Stack(app, 'ec2', env=selected_env, vpc=vpc.vpc, config=config)
 
 app.synth()
