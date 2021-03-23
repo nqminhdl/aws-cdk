73974264a9ececbb299b7bbb3a27faa708077bbc
Minh Nguyen <minh.nguyen@cxagroup.com>
2021-03-24 06:52:41 +0700

    Add code to provision cloudfront

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from webapp.ec2 import Ec2Stack
 from webapp.autoscaling import AutoscalingStack
 from webapp.rds import RDSStack
 from webapp.s3 import S3Stack
+from webapp.cloudfront import CloudFrontStack
 
 selected_env = core.Environment(account="658564819138", region="ap-southeast-1")
 
@@ -20,6 +21,7 @@ ec2 = Ec2Stack(app, 'ec2', env=selected_env, vpc=vpc.vpc, config=config)
 autoscaling = AutoscalingStack(app, 'autoscaling', env=selected_env, vpc=vpc.vpc, bastion=ec2.bastion, config=config)
 rds = RDSStack(app, 'rds', vpc=vpc.vpc, env=selected_env)
 s3  = S3Stack(app, 's3', env=selected_env)
+cloudfront = CloudFrontStack(app, 'cloudfront', frontend_bucket=core.Fn.import_value('frontend-bucket-name'), env=selected_env)
 
 core.Tags.of(app).add("Managed-By", "DevOps")
 core.Tags.of(app).add("Provisioned-By", "AWS CDK")
